d3d8647c7f3a6ef362aaff31540432067206c731
FaisalMohammedElorm <mohammedelormf@gmail.com>
2025-11-08 17:27:37 +0000

    splash screen done

--- app/splash.tsx
+++ app/splash.tsx
@@ -1,12 +1,74 @@
-import { View, Text } from 'react-native'
 import React from 'react'
+import { ImageBackground, StyleSheet, Text, View, TouchableOpacity } from 'react-native'
 
 const Splash = () => {
   return (
-    <View>
-      <Text>Splash</Text>
+    <View style={styles.container}>
+      <ImageBackground 
+        source={require('../assets/images/Coffee.png')} 
+        style={styles.backgroundImage}
+      >
+      <View style={styles.contentContainer}>
+        <Text style={styles.mainText}>Fall in Love with </Text>
+          <Text style={styles.mainText}>Coffee in Blissful </Text>
+            <Text style={styles.mainText}>Delight!</Text>
+            <Text style={styles.subText}>Welcome to our cozy coffee corner, where </Text>
+              <Text style={styles.subText}>every cup is a delightful for you.</Text>
+      </View>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Get Started</Text>
+      </TouchableOpacity>
+      </ImageBackground>
+      
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  backgroundImage: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  contentContainer: {
+    // flex: 0.5,
+    marginTop: 450,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#05050500',
+    // alignContent: 'space-between',
+  },
+  mainText: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    color: '#FFFFFF',
+    lineHeight: 40,
+    letterSpacing: 1,
+  },
+  subText: {
+    top: 38,
+    fontSize: 16,
+    color: '#A2A2A2',
+    fontWeight: '600',
+  },
+  buttonText: {
+    fontSize: 16,
+    color: '#FFFFFF',
+    fontWeight: 'bold',
+  },
+  button: {
+    paddingVertical: 12,
+    paddingHorizontal: 24,
+    borderRadius: 15,
+    backgroundColor: '#C67C4E',
+    top:100,
+    width: '90%',
+    height: 50,
+    alignItems: 'center',
+  }
+})
 
 export default Splash
