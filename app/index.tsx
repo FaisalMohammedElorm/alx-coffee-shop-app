86e7425a429149bd6b2e37af3fe648c15da11582
FaisalMohammedElorm <mohammedelormf@gmail.com>
2025-11-08 16:45:25 +0000

    layout

---
+++ app/index.tsx
@@ -0,0 +1,11 @@
+// import { View, Text } from 'react-native'
+import React from 'react'
+import Splash from './splash'
+
+const Index = () => {
+  return (
+   <Splash />
+  )
+}
+
+export default Index  
